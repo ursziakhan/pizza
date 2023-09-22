57bfb0e4e64b02d63bf6f28ea9f1487381cfeb41
Bilal Zahid <88459741+bilalzhd@users.noreply.github.com>
2023-09-23 02:52:22 +0500

    fix file uploading

--- components/ProductForm.js
+++ components/ProductForm.js
@@ -1,120 +1,118 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { useRouter } from 'next/router';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function ProductForm() {
-    const [title, setTitle] = useState('');
-    const [description, setDescription] = useState('');
-    const [selectedFiles, setSelectedFiles] = useState([]);
-    const [price, setPrice] = useState('');
-    const [goToProducts, setGoToProducts] = useState(false);
-    const [categories, setCategories] = useState([]);
-    const router = useRouter();
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [selectedImage, setSelectedImage] = useState("");
+  const [selectedFile, setSelectedFile] = useState();
 
-    
-    // fixing categories
-    useEffect(() => {
-      axios.get('/api/categories').then(result =>{ 
-        setCategories(result.data);
+  const [price, setPrice] = useState("");
+  const [goToProducts, setGoToProducts] = useState(false);
+  const [categories, setCategories] = useState([]);
+
+  // fixing categories
+  useEffect(() => {
+    axios.get("/api/categories").then((result) => {
+      setCategories(result.data);
+    });
+  }, []);
+
+  const handleChange = (e) => {
+    setTitle(e.target.value);
+  };
+
+  async function createProduct(event) {
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append("files", selectedFile);
+
+    try {
+      const uploadResponse = await fetch("/api/upload", {
+        method: "POST",
+        body: formData,
       });
-    }, []);
 
+      if (!uploadResponse.ok) {
+        console.error("Failed to upload images");
+        return;
+      }
 
-    const handleChange = (e) => {
-        setTitle(e.target.value);
-    };
+      console.log(uploadResponse.body);
 
-    const handleFileChange = (event) => {
-        const files = event.target.files;
-        setSelectedFiles(files);
-    };
+      // Assuming the API route returns file information in JSON format
+      const { files } = await uploadResponse.json();
+      const productData = {
+        title,
+        description,
+        price,
+        images: files, // Use the file information received from the API
+      };
 
-    async function createProduct(event) {
-        event.preventDefault();
+      // Create the product using the file information
+      await axios.post("/api/products", productData);
+      setGoToProducts(true);
+    } catch (error) {
+      console.error("Error uploading images:", error);
+    }
+  }
 
-        const formData = new FormData();
-        for (const file of selectedFiles) {
-            formData.append('files', file);
-        }
+  //   if (goToProducts) {
+  //     router.push("/products/");
+  //   }
+  return (
+    <form onSubmit={createProduct}>
+      <label>Product name</label>
+      <input
+        required
+        value={title}
+        type="text"
+        placeholder="Product name"
+        onChange={handleChange}
+      />
+      <label>Category</label>
+      <select>
+        <option value="">Uncategorized</option>
+        {categories.length > 0 &&
+          categories.map((cat, index) => (
+            <option key={index} value={cat._id}>
+              {cat.name}
+            </option>
+          ))}
+      </select>
 
-        try {
-            const imageResponse = await fetch('/api/upload', {
-                method: 'POST',
-                body: formData,
-            });
-
-            if (!imageResponse.ok) {
-                console.error('Failed to upload images');
-                return;
+      <label>Price in Sek</label>
+      <input
+        required
+        value={price}
+        placeholder="price"
+        type="number"
+        onChange={(e) => setPrice(e.target.value)}
+      />
+      <label>Product description</label>
+      <textarea
+        required
+        value={description}
+        placeholder="Description"
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <label htmlFor="images">Upload an image</label>
+      <div className="mb-2">
+        <input
+          id="image"
+          type="file"
+          onChange={(e) => {
+            if (e.target.files) {
+              const file = e.target.files[0];
+              // setSelectedImage(URL.createObjectURL(file));
+              setSelectedFile(file);
             }
-
-            const { filenames } = await imageResponse.json();
-
-            const productData = {
-                title,
-                description,
-                price,
-                images: filenames,
-            };
-
-            await axios.post('/api/products', productData);
-            setGoToProducts(true);
-        } catch (error) {
-            console.error('Error uploading images:', error);
-        }
-    }
-
-    if (goToProducts) {
-        router.push('/products/');
-    }
-
-    return (
-        <form onSubmit={createProduct}>
-            <label>Product name</label>
-            <input
-                required
-                value={title}
-                type="text"
-                placeholder="Product name"
-                onChange={handleChange}
-            />
-    <label>Category</label>
-   <select>
-  <option value="">Uncategorized</option>
-  {categories.length > 0 && categories.map((cat, index) => (
-    <option key={index} value={cat._id}>{cat.name}</option>
-  ))}
-</select>
-
-
-
-
-            <label>Price in Sek</label>
-            <input
-                required
-                value={price}
-                placeholder="price"
-                type="number"
-                onChange={(e) => setPrice(e.target.value)}
-            />
-            <label>Product description</label>
-            <textarea
-                required
-                value={description}
-                placeholder="Description"
-                onChange={(e) => setDescription(e.target.value)}
-            />
-            <label htmlFor="images">Upload an image</label>
-            <div className="mb-2">
-                <input
-                    id="image"
-                    type="file"
-                    onChange={handleFileChange}
-                />
-            </div>
-            <button type="submit" className="btn-primary">
-                Submit
-            </button>
-        </form>
-    );
+          }}
+        />
+      </div>
+      <button type="submit" className="btn-primary">
+        Submit
+      </button>
+    </form>
+  );
 }
